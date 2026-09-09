bbed3aa5028f435095dbcd2ce7f6459b0eee2a6e
Alexandru Ursu <apetrut7@gmail.com>
2026-09-09 10:48:36 +0300

    backend structure + main controller

---
+++ backend/src/main/java/org/schemeguard/backend/controller/MainController.java
@@ -0,0 +1,14 @@
+package org.schemeguard.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping({"/", "/home"})
+    public String home() {
+        return "Hello, World!";
+    }
+
+}
